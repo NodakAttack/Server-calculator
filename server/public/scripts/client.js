bf0585fcac40c6fa0112bba2a3d753fe1220a310
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-08-02 11:59:47 -0500

    send operation object to server via post request on = button click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ let operator = null;
 
 function submitForm(event){
     event.preventDefault();
+    // convert values to number?
     let number1 = document.querySelector("#number1").value;
     let number2 = document.querySelector("#number2").value;
 
@@ -13,10 +14,19 @@ function submitForm(event){
         number2: number2,
     }
 
-    
+    axios.post('/calc', operationToAdd).then((response) => {
+        console.log(response);
+        document.querySelector("#number1").value = '';
+        document.querySelector("#number2").value = '';
+            // get calcs goes here
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong 1')
+    })
     console.log('operation ', operationToAdd);
 }
 
 function operatorClick(operatorFromHTML){
     operator = operatorFromHTML;
 }
+
--- server/server.js
+++ server/server.js
@@ -3,7 +3,22 @@ let app = express();
 const port = 5001;
 app.use(express.json());
 
+let calcList = [];
+
 app.use(express.static('server/public'));
+
+app.post('/calc', (req,res) => {
+    console.log('get a POST request.', req.body);;
+
+    let calc = req.body;
+    calcList.push(calc);
+    res.sendStatus(201);
+})
+
+
+
+
+
 
 app.listen(port, function() { 
     console.log('listening on port', port);
