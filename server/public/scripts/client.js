f13e8e4615a6674e6dae35daffd3483704e86b74
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-08-02 11:38:31 -0500

    added operator variable, when HTML operation is clicked, assigned a value in js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,21 +1,22 @@
 console.log('script sourced =)');
 
+let operator = null;
+
 function submitForm(event){
     event.preventDefault();
     let number1 = document.querySelector("#number1").value;
     let number2 = document.querySelector("#number2").value;
-    let operator = 
 
     let operationToAdd = {
         number1: number1,
+        operator: operator,
         number2: number2,
     }
 
+    
     console.log('operation ', operationToAdd);
-
 }
 
-function operatorClick(){
-    
-
+function operatorClick(operatorFromHTML){
+    operator = operatorFromHTML;
 }
