9ca4eaef241101c304110ec625c99b97a17fb8c3
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-08-02 11:23:50 -0500

    got number input values from DOM, put in object to send

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,21 @@
 console.log('script sourced =)');
 
+function submitForm(event){
+    event.preventDefault();
+    let number1 = document.querySelector("#number1").value;
+    let number2 = document.querySelector("#number2").value;
+    let operator = 
+
+    let operationToAdd = {
+        number1: number1,
+        number2: number2,
+    }
+
+    console.log('operation ', operationToAdd);
+
+}
+
+function operatorClick(){
+    
+
+}
