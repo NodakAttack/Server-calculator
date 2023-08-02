d89c2a0292103669e46081e9c95c1b54913c038a
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-08-02 14:24:32 -0500

    formatted code, finished comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,24 +1,27 @@
 console.log('script sourced =)');
 
+// operator pushed
 let operator = null;
 
-function submitForm(event){
+// onClick of = button
+function submitForm(event) {
     event.preventDefault();
-    // convert values to number?
     let number1 = Number(document.querySelector("#number1").value);
     let number2 = Number(document.querySelector("#number2").value);
 
+        // object to be passed to server
     let operationToAdd = {
         number1: number1,
         operator: operator,
         number2: number2,
     }
 
+        // reset input, pass object to server via post
     axios.post('/calc', operationToAdd).then((response) => {
         console.log(response);
         document.querySelector("#number1").value = '';
         document.querySelector("#number2").value = '';
-            getCalcs();
+        getCalcs();
     }).catch((error) => {
         console.log(error);
         alert('Something went wrong 1')
@@ -26,11 +29,12 @@ function submitForm(event){
     console.log('operation ', operationToAdd);
 }
 
-
-function getCalcs(){
+    // get request of calcList from server
+function getCalcs() {
     axios.get('/calc').then((response) => {
         console.log('success', response);
         let calcsFromServer = response.data;
+            // renderToDOM function of calcList from server
         renderToDOM(calcsFromServer)
         console.log('calcs from server', calcsFromServer);
     }).catch((error) => {
@@ -39,32 +43,38 @@ function getCalcs(){
     })
 }
 
-function renderToDOM(calcs){
+    // Displays recent result and list of calculations
+function renderToDOM(calcs) {
     let outputList = document.querySelector('#output');
     outputList.innerHTML = `<h2>${calcs.at(-1).result}</h2>`
 
-    for(let calc of calcs) {
+    for (let calc of calcs) {
         outputList.innerHTML += `
             <p>${calc.number1} ${calc.operator} ${calc.number2} = ${calc.result}</p>
         `
     }
 }
 
-function operatorClick(operatorFromHTML){
+// handles which operator is clicked, setting button color to red and all others to default
+function operatorClick(operatorFromHTML) {
+        // operator selected, sent to operationToAdd
     operator = operatorFromHTML;
+        // all operator buttons put in an array
     const operatorButtons = document.querySelectorAll(".operator");
-
+        // listener event added to buttons if clicked
     operatorButtons.forEach(button => {
-      button.addEventListener("click", function(event) {
-        // set the background color of all buttons 
-        operatorButtons.forEach(button => {
-          button.style.backgroundColor = '';
-        });
+        button.addEventListener("click", function (event) {
+            // set the background color of all buttons 
+            operatorButtons.forEach(button => {
+                button.style.backgroundColor = '';
+            });
             // change the listener clicked 
-        event.target.style.backgroundColor = 'red';
-      });
+            event.target.style.backgroundColor = 'red';
+        });
     });
 }
+
+// outdated function to change button color
 
 // function changeButtonColor(button, color) {
 //     button.style.backgroundColor = color;
--- server/server.js
+++ server/server.js
@@ -3,17 +3,21 @@ let app = express();
 const port = 5001;
 app.use(express.json());
 
+// list of calculations, array of objects
+    // number1:
+    // operator:
+    // number2:
+    // result:
 let calcList = [];
 
 app.use(express.static('server/public'));
 
+// post request getting calc object from client
 app.post('/calc', (req,res) => {
     console.log('get a POST request.', req.body);;
-
     let calc = req.body;
-
+    // switch case does calculation and gets calc.result depending on operation submitted
     let result;
-
     switch (calc.operator) {
         case "+":
             result = calc.number1 + calc.number2;
@@ -38,6 +42,7 @@ app.post('/calc', (req,res) => {
     res.sendStatus(201);
 })
 
+// get response of calcList
 app.get('/calc', function(req, res){
     console.log('request for /calc was made');
     res.send(calcList)
@@ -45,7 +50,7 @@ app.get('/calc', function(req, res){
 
 
 
-
+ // listening on port 5001
 app.listen(port, function() { 
     console.log('listening on port', port);
 })
